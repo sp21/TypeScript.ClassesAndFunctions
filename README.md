e19e1b0f17f5321a632783f9420c2f8b1f3e2eef
sp21 <pshreyasgowda1997@gmail.com>
2020-06-25 20:24:44 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-w2gkew
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-w2gkew)
---
+++ index.ts
@@ -0,0 +1,18 @@
+class Customer{
+  custName:string;
+  className:string;
+  age:number;
+  constructor(custName:string,className:string,age:number){
+    this.custName=custName;
+    className=className;
+    this.age=age;
+  }
+  hello():Boolean{
+  console.log("hello shreyas")
+  return true;
+}
+  
+}
+var cust1:Customer =new Customer("shreyas","A",21);
+console.log(cust1.custName);
+cust1.hello();
